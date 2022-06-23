e7aeadf27c63e6a679c045e9ceead49150998a78
mv.kaznach <mv.kaznach@gmail.com>
2022-06-23 23:26:10 +0300

    feat: added tests to field

--- src/field.rs
+++ src/field.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct FieldElement {
     num: u32,
     order: u32,
@@ -6,7 +6,9 @@ pub struct FieldElement {
 
 impl FieldElement {
     pub fn new(num: u32, order: u32) -> Self {
-        assert!(num <= order, "Field order must be geather than num");
+        if num >= order {
+            panic!("Field order must be geather than num");
+        }
 
         FieldElement { num, order }
     }
@@ -20,7 +22,9 @@ impl std::ops::Add<FieldElement> for FieldElement {
     type Output = FieldElement;
 
     fn add(self, rhs: FieldElement) -> FieldElement {
-        assert!(self.num != rhs.num, "The order of fields must be equal");
+        if self.order != rhs.order {
+            panic!("The order of fields must be equal");
+        }
 
         FieldElement::new((self.num + rhs.num) % self.order, self.order)
     }
@@ -30,7 +34,9 @@ impl std::ops::Mul<FieldElement> for FieldElement {
     type Output = FieldElement;
 
     fn mul(self, rhs: FieldElement) -> Self::Output {
-        assert!(self.num != rhs.num, "The order of fields must be equal");
+        if self.order != rhs.order {
+            panic!("The order of fields must be equal");
+        }
 
         FieldElement::new((rhs.num * self.num) % rhs.order, rhs.order)
     }
@@ -40,9 +46,77 @@ impl std::ops::Div<FieldElement> for FieldElement {
     type Output = FieldElement;
 
     fn div(self, rhs: FieldElement) -> Self::Output {
-        assert!(self.num != rhs.num, "The order of fields must be equal");
+        if self.order != rhs.order {
+            panic!("The order of fields must be equal");
+        }
 
         let back_rhs = rhs.pow(self.order - 2);
         self * back_rhs
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::FieldElement;
+
+    #[test]
+    #[should_panic(expected = "Field order must be geather than num")]
+    fn test_new_panic() {
+        _ = FieldElement::new(10, 1);
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(
+            FieldElement::new(5, 8),
+            FieldElement::new(3, 8) + FieldElement::new(2, 8)
+        );
+        assert_eq!(
+            FieldElement::new(4, 8),
+            FieldElement::new(5, 8) + FieldElement::new(7, 8)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "The order of fields must be equal")]
+    fn test_add_panic() {
+        _ = FieldElement::new(1, 2) + FieldElement::new(2, 3)
+    }
+
+    #[test]
+    fn test_mul() {
+        assert_eq!(
+            FieldElement::new(6, 8),
+            FieldElement::new(3, 8) * FieldElement::new(2, 8)
+        );
+        assert_eq!(
+            FieldElement::new(4, 8),
+            FieldElement::new(3, 8) * FieldElement::new(4, 8)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "The order of fields must be equal")]
+    fn test_mul_panic() {
+        _ = FieldElement::new(1, 2) * FieldElement::new(2, 3)
+    }
+
+    #[test]
+    fn test_pow() {
+        assert_eq!(FieldElement::new(3, 8), FieldElement::new(3, 8).pow(3))
+    }
+
+    #[test]
+    fn test_div() {
+        assert_eq!(
+            FieldElement::new(2, 4),
+            FieldElement::new(2, 4) / FieldElement::new(3, 4)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "The order of fields must be equal")]
+    fn test_div_panic() {
+        _ = FieldElement::new(1, 2) / FieldElement::new(2, 3)
     }
 }
